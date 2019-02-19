0c9fc0c1f8e5913880762a6f2ba051108c316f07
david-frederick <j.david.frederick.57@gmail.com>
2019-02-19 16:10:02 -0500

    gets a sample Sidekiq worker running and writing to AirTable

---
+++ app/workers/hard_worker.rb
@@ -0,0 +1,11 @@
+class HardWorker
+  include Sidekiq::Worker
+
+  def perform(*args)
+    # Pass in api key to client
+    at_client = Airtable::Client.new('keyTycbrAXKjR7VPu')
+    at_test   = at_client.table('appPJ6HUieDnNWxOi', 'Test')
+    record    = Airtable::Record.new('Name' => 'Thing1')
+    at_test.create(record)
+  end
+end
