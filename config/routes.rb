2c5c0bf593a86529cd0d9f0bf7b0da16fddb34aa
david-frederick <j.david.frederick.57@gmail.com>
2019-04-11 15:42:02 -0400

    UI cleanup and basic shops view/controller setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :games, only: [:index, :show]
+  resources :shops, only: [:index, :show]
+
+  root 'games#index'
 end
