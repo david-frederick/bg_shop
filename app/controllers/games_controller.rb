0fbf1d34aa81842bb37ca53aeb50066c7ceed913
david-frederick <j.david.frederick.57@gmail.com>
2019-04-16 14:50:21 -0400

    show shop_games and minor UI tweaks

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,14 @@
 class GamesController < ApplicationController
   def index
-    @games = Game.complete.all
+    @games = Game.complete.all.sort_by { |game| game.name }
   end
 
   def show
-    @game = Game.find(params[:id])
+    @game  = Game.find(params[:id])
+    @shop_games = @game.shop_games
+    @shop_games = @shop_games.to_a.map { |shop_game| { url: shop_game.url, shop: shop_game.shop } }
+
+    puts '--'
+    ap @shop_games.first
   end
 end
